a8b3ba248cde74cfb7f028b4e1a3a557fc14fbaa
TobMit <80719513+tobmit@users.noreply.github.com>
2022-11-09 10:02:42 +0100

    Základné nastavenie frameworku

--- App/Config/Configuration.php
+++ App/Config/Configuration.php
@@ -11,13 +11,13 @@ use App\Auth\DummyAuthenticator;
  */
 class Configuration
 {
-    public const APP_NAME = 'Vaííčko MVC FW';
+    public const APP_NAME = 'Movie Database';
     public const FW_VERSION = '2.0';
 
     public const DB_HOST = 'localhost';  // change to db, if docker you use docker
-    public const DB_NAME = 'vaiicko_db';
-    public const DB_USER = 'root'; // change to vaiicko_user, if docker you use docker
-    public const DB_PASS = 'dtb456';
+    public const DB_NAME = 'MovDB';
+    public const DB_USER = 'mov_user'; // change to vaiicko_user, if docker you use docker
+    public const DB_PASS = 'db_user_pass';
 
     public const LOGIN_URL = '?c=auth&a=login';
 
