8e9bb31c3e7ae656b389ed8bf72360782862bb52
TobMit <80719513+tobmit@users.noreply.github.com>
2022-11-14 01:39:57 +0100

    JS funguje

--- App/Views/Home/index.view.php
+++ App/Views/Home/index.view.php
@@ -59,127 +59,32 @@
     </div>
 </div>
 <div class="row justify-content-evenly" id="sugestedMoviesSerials">
+<!--
+    <div class="col-md-2 border rounded-4 m-1 ">
+        <a href="?c=movie&a=title">
+            <img class="image w-100 rounded-4 mt-3" src="https://m.media-amazon.com/images/M/MV5BMmVlODAyNTAtODc3Yi00MjFhLTk5MTktNWIwOTUzY2M2ZDc5XkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_FMjpg_UX1000_.jpg" alt="The Rings of Power plagat">
+        </a>
+        <div class="nazov text-center text-white">
+            The Rings of Power
+        </div>
+    </div>
+    <div class="col-md-2 border rounded-4 m-1 ">
+        <a href="?c=movie&a=title">
+            <img class=" w-100 rounded-4 mt-3" src="https://m.media-amazon.com/images/M/MV5BNzhlNTAyY2YtNWUyYi00YmE3LWE2OTctZDI2MmJkNmZiMmY0XkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_.jpg" alt="Luck plagat">
+        </a>
+        <div class="nazov text-center ">
+            Luck
+        </div>
+    </div>
+    <div class="col-md-2 border rounded-4 m-1 ">
+        <a href="?c=movie&a=title">
+            <img class="image w-100 rounded-4 mt-3" src="https://m.media-amazon.com/images/M/MV5BOGNmM2IyMjMtNmU0MC00MzdhLWFlNWYtOTlkZDdiOGEwMTE3XkEyXkFqcGdeQXVyMTA3MDk2NDg2._V1_.jpg" alt="Memory plagat">
+        </a>
+        <div class="nazov text-center ">
+            Memory
+        </div>
+    </div>
+    -->
 
-    <div class="col-md-2 border rounded-4 m-1 ">
-        <a href="?c=movie&a=title">
-            <img class="image w-100 rounded-4 mt-3" src="https://m.media-amazon.com/images/M/MV5BMmVlODAyNTAtODc3Yi00MjFhLTk5MTktNWIwOTUzY2M2ZDc5XkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_FMjpg_UX1000_.jpg" alt="The Rings of Power plagat">
-        </a>
-        <div class="nazov text-center text-white">
-            The Rings of Power
-        </div>
-    </div>
-    <div class="col-md-2 border rounded-4 m-1 ">
-        <a href="?c=movie&a=title">
-            <img class=" w-100 rounded-4 mt-3" src="https://m.media-amazon.com/images/M/MV5BNzhlNTAyY2YtNWUyYi00YmE3LWE2OTctZDI2MmJkNmZiMmY0XkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_.jpg" alt="Luck plagat">
-        </a>
-        <div class="nazov text-center ">
-            Luck
-        </div>
-    </div>
-    <div class="col-md-2 border rounded-4 m-1 ">
-        <a href="?c=movie&a=title">
-            <img class="image w-100 rounded-4 mt-3" src="https://m.media-amazon.com/images/M/MV5BOGNmM2IyMjMtNmU0MC00MzdhLWFlNWYtOTlkZDdiOGEwMTE3XkEyXkFqcGdeQXVyMTA3MDk2NDg2._V1_.jpg" alt="Memory plagat">
-        </a>
-        <div class="nazov text-center ">
-            Memory
-        </div>
-    </div>
-
-    <div class="col-md-2 border rounded-4 m-1 ">
-        <a href="?c=movie&a=title">
-            <img class="image w-100 rounded-4 mt-3" src="https://m.media-amazon.com/images/M/MV5BMmVlODAyNTAtODc3Yi00MjFhLTk5MTktNWIwOTUzY2M2ZDc5XkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_FMjpg_UX1000_.jpg" alt="The Rings of Power plagat">
-        </a>
-        <div class="nazov text-center text-white">
-            The Rings of Power
-        </div>
-    </div>
-    <div class="col-md-2 border rounded-4 m-1 ">
-        <a href="?c=movie&a=title">
-            <img class=" w-100 rounded-4 mt-3" src="https://m.media-amazon.com/images/M/MV5BNzhlNTAyY2YtNWUyYi00YmE3LWE2OTctZDI2MmJkNmZiMmY0XkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_.jpg" alt="Luck plagat">
-        </a>
-        <div class="nazov text-center ">
-            Luck
-        </div>
-    </div>
-    <div class="col-md-2 border rounded-4 m-1 ">
-        <a href="?c=movie&a=title">
-            <img class="image w-100 rounded-4 mt-3" src="https://m.media-amazon.com/images/M/MV5BOGNmM2IyMjMtNmU0MC00MzdhLWFlNWYtOTlkZDdiOGEwMTE3XkEyXkFqcGdeQXVyMTA3MDk2NDg2._V1_.jpg" alt="Memory plagat">
-        </a>
-        <div class="nazov text-center ">
-            Memory
-        </div>
-    </div>
-    <div class="col-md-2 border rounded-4 m-1 ">
-        <a href="?c=movie&a=title">
-            <img class="image w-100 rounded-4 mt-3" src="https://m.media-amazon.com/images/M/MV5BMmVlODAyNTAtODc3Yi00MjFhLTk5MTktNWIwOTUzY2M2ZDc5XkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_FMjpg_UX1000_.jpg" alt="The Rings of Power plagat">
-        </a>
-        <div class="nazov text-center text-white">
-            The Rings of Power
-        </div>
-    </div>
-    <div class="col-md-2 border rounded-4 m-1 ">
-        <a href="?c=movie&a=title">
-            <img class=" w-100 rounded-4 mt-3" src="https://m.media-amazon.com/images/M/MV5BNzhlNTAyY2YtNWUyYi00YmE3LWE2OTctZDI2MmJkNmZiMmY0XkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_.jpg" alt="Luck plagat">
-        </a>
-        <div class="nazov text-center ">
-            Luck
-        </div>
-    </div>
-    <div class="col-md-2 border rounded-4 m-1 ">
-        <a href="?c=movie&a=title">
-            <img class="image w-100 rounded-4 mt-3" src="https://m.media-amazon.com/images/M/MV5BOGNmM2IyMjMtNmU0MC00MzdhLWFlNWYtOTlkZDdiOGEwMTE3XkEyXkFqcGdeQXVyMTA3MDk2NDg2._V1_.jpg" alt="Memory plagat">
-        </a>
-        <div class="nazov text-center ">
-            Memory
-        </div>
-    </div>
-    <div class="col-md-2 border rounded-4 m-1 ">
-        <a href="?c=movie&a=title">
-            <img class="image w-100 rounded-4 mt-3" src="https://m.media-amazon.com/images/M/MV5BMmVlODAyNTAtODc3Yi00MjFhLTk5MTktNWIwOTUzY2M2ZDc5XkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_FMjpg_UX1000_.jpg" alt="The Rings of Power plagat">
-        </a>
-        <div class="nazov text-center text-white">
-            The Rings of Power
-        </div>
-    </div>
-    <div class="col-md-2 border rounded-4 m-1 ">
-        <a href="?c=movie&a=title">
-            <img class=" w-100 rounded-4 mt-3" src="https://m.media-amazon.com/images/M/MV5BNzhlNTAyY2YtNWUyYi00YmE3LWE2OTctZDI2MmJkNmZiMmY0XkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_.jpg" alt="Luck plagat">
-        </a>
-        <div class="nazov text-center ">
-            Luck
-        </div>
-    </div>
-    <div class="col-md-2 border rounded-4 m-1 ">
-        <a href="?c=movie&a=title">
-            <img class="image w-100 rounded-4 mt-3" src="https://m.media-amazon.com/images/M/MV5BOGNmM2IyMjMtNmU0MC00MzdhLWFlNWYtOTlkZDdiOGEwMTE3XkEyXkFqcGdeQXVyMTA3MDk2NDg2._V1_.jpg" alt="Memory plagat">
-        </a>
-        <div class="nazov text-center ">
-            Memory
-        </div>
-    </div>
-    <div class="col-md-2 border rounded-4 m-1 ">
-        <a href="?c=movie&a=title">
-            <img class="image w-100 rounded-4 mt-3" src="https://m.media-amazon.com/images/M/MV5BMmVlODAyNTAtODc3Yi00MjFhLTk5MTktNWIwOTUzY2M2ZDc5XkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_FMjpg_UX1000_.jpg" alt="The Rings of Power plagat">
-        </a>
-        <div class="nazov text-center text-white">
-            The Rings of Power
-        </div>
-    </div>
-    <div class="col-md-2 border rounded-4 m-1 ">
-        <a href="?c=movie&a=title">
-            <img class=" w-100 rounded-4 mt-3" src="https://m.media-amazon.com/images/M/MV5BNzhlNTAyY2YtNWUyYi00YmE3LWE2OTctZDI2MmJkNmZiMmY0XkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_.jpg" alt="Luck plagat">
-        </a>
-        <div class="nazov text-center ">
-            Luck
-        </div>
-    </div>
-    <div class="col-md-2 border rounded-4 m-1 ">
-        <a href="?c=movie&a=title">
-            <img class="image w-100 rounded-4 mt-3" src="https://m.media-amazon.com/images/M/MV5BOGNmM2IyMjMtNmU0MC00MzdhLWFlNWYtOTlkZDdiOGEwMTE3XkEyXkFqcGdeQXVyMTA3MDk2NDg2._V1_.jpg" alt="Memory plagat">
-        </a>
-        <div class="nazov text-center ">
-            Memory
-        </div>
-    </div>
 
 </div>
--- public/js/script.js
+++ public/js/script.js
@@ -3,14 +3,41 @@
 // TODO Pre uplnú funkčnosť treba vytvoriť ešte js file s nazvom config kde uložíme pomocou premennej uložíme vlastný API_KEY
 
 const API_KEY_URL = 'api_key=' + API_KEY;
-const BASE_URL = 'https://api.themoviedb.org/3/';
+const BASE_URL = 'https://api.themoviedb.org/3';
 const POP_MOVIE = '/trending/all/week?';
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 // BASE_REQUEST = BASE_URL + "request" + API_KEY
 
 getMovies(BASE_URL+POP_MOVIE+API_KEY_URL);
 
 function getMovies(url) {
+    //zíkame url request, response uložime do res.json a potom prístpime k dátam
     fetch(url).then(res => res.json()).then(data => {
         console.log(data);
+
+        //aby sme nemuseli parsovať všetko iba result object
+        showMovies(data.results);
+    })
+}
+
+function showMovies(data) {
+    data.forEach(movie => {
+        // získame tieto informácie z fore cyklu a vyhľadávame ich podla názvu v json
+        const {title, poster_path, name } = movie;
+        //filmi a serialy majú rôzne označenie názvu tak toto to rieši
+        if (typeof title ==="undefined") {
+            nazov = name;
+        } else {
+            nazov = title;
+        }
+        const movieElement = document.getElementById("sugestedMoviesSerials");
+        movieElement.innerHTML += `<div class="col-md-2 border rounded-4 m-1 ">\n` +
+                                `        <a href="?c=movie&a=title">\n` +
+                                `            <img class="image w-100 rounded-4 mt-3" src="${IMG_URL+poster_path}" alt="${nazov}">\n` +
+                                `        </a>\n` +
+                                `        <div class="nazov text-center text-white">\n` +
+                                `            ${nazov}\n` +
+                                `        </div>\n` +
+                                `    </div> `;
     })
 }
