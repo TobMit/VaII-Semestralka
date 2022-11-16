13db31b0753a4d8babd4aa3743a3333264b65599
TobMit <80719513+tobmit@users.noreply.github.com>
2022-11-16 15:56:53 +0100

    Model pre databázu

---
+++ App/Models/Users.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Models;
+
+use App\Core\Model;
+
+class Users extends Model
+{
+    protected $id;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserName()
+    {
+        return $this->user_name;
+    }
+
+    /**
+     * @param mixed $user_name
+     */
+    public function setUserName($user_name): void
+    {
+        $this->user_name = $user_name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password): void
+    {
+        $this->password = $password;
+    }
+    protected $user_name;
+    protected $email;
+    protected $password;
+
+}
