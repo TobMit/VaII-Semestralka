50b785920b9a1a80a48b2967a27e5c11f22f5321
TobMit <80719513+tobmit@users.noreply.github.com>
2023-01-13 16:56:30 +0100

    Nová tabulka pridaná do Systému

---
+++ App/Models/Comments.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Models;
+
+class Comments extends \App\Core\Model
+{
+    protected $id;
+    protected $users;
+    protected $text;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsers()
+    {
+        return $this->users;
+    }
+
+    /**
+     * @param mixed $users
+     */
+    public function setUsers($users): void
+    {
+        $this->users = $users;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param mixed $text
+     */
+    public function setText($text): void
+    {
+        $this->text = $text;
+    }
+}
